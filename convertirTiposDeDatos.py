4de5d1101ae7387a49d12e38e86b769b6464ef0d
diegoziraldo <ziraldodiego@gmail.com>
2024-10-03 00:02:14 -0300

    031024

---
+++ convertirTiposDeDatos.py
@@ -0,0 +1,15 @@
+
+# x = 1
+# y = 2.0
+# z = '3'
+
+
+# print(type(int(x)))
+# print(type(str(y)))
+# print(type(z))
+
+################################################
+
+
+# edad = 24
+# print("Edad: " + str(edad))
---
+++ metodosParaCadenas.py
@@ -0,0 +1,11 @@
+nombre = "Diego"
+# print(len(nombre))
+# print(nombre.find('i'))
+# print(nombre.capitalize())
+# print(nombre.upper())
+# print(nombre.lower())
+# print(nombre.isdigit())
+# print(nombre.isalpha())
+# print(nombre.count('i'))
+# print(nombre.replace("g", "8"))
+# print(nombre * 3)
