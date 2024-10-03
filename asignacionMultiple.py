7c2092782d494ef819c985303b4b0bc290eaac6b
diegoziraldo <ziraldodiego@gmail.com>
2024-10-02 23:44:07 -0300

    021024

---
+++ asignacionMultiple.py
@@ -0,0 +1,24 @@
+# nombre = 'Diego'
+# edad = 39
+# atractivo = True
+
+# nombre, edad, atractivo = 'Diego', 39, True
+
+# print(nombre)
+# print(edad)
+# print(atractivo)  
+
+############################################################################################
+
+
+# nombre1 = 10
+# nombre2 = 10
+# nombre3 = 10
+# nombre4 = 10
+
+# nombre1 = nombre2 = nombre3 = nombre4 = 10
+
+# print(nombre1)
+# print(nombre2)
+# print(nombre3)
+# print(nombre4)
